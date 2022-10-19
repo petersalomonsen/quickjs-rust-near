1cc712f0c8fd753767c16ab82c75b67c50b48247
Peter Salomonsen <pjsalomonsen@gmail.com>
2022-10-19 20:38:25 +0200

    nft_metadata

--- examples/nft/src/lib.rs
+++ examples/nft/src/lib.rs
@@ -8,11 +8,9 @@ use near_sdk::collections::LazyOption;
 use near_sdk::{
     env, near_bindgen, AccountId, BorshStorageKey, PanicOnDefault, Promise, PromiseOrValue,
 };
-use quickjs_rust_near::web4::{
-    types::{Web4Request, Web4Response}
-};
+use quickjs_rust_near::web4::types::{Web4Request, Web4Response};
 mod web4content;
-use web4content::{INDEX_HTML,MUSIC_WASM,SERVICEWORKER};
+use web4content::{INDEX_HTML, MUSIC_WASM, SERVICEWORKER};
 
 const DATA_IMAGE_SVG_NEAR_ICON: &str = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 288 288'%3E%3Cg id='l' data-name='l'%3E%3Cpath d='M187.58,79.81l-30.1,44.69a3.2,3.2,0,0,0,4.75,4.2L191.86,103a1.2,1.2,0,0,1,2,.91v80.46a1.2,1.2,0,0,1-2.12.77L102.18,77.93A15.35,15.35,0,0,0,90.47,72.5H87.34A15.34,15.34,0,0,0,72,87.84V201.16A15.34,15.34,0,0,0,87.34,216.5h0a15.35,15.35,0,0,0,13.08-7.31l30.1-44.69a3.2,3.2,0,0,0-4.75-4.2L96.14,186a1.2,1.2,0,0,1-2-.91V104.61a1.2,1.2,0,0,1,2.12-.77l89.55,107.23a15.35,15.35,0,0,0,11.71,5.43h3.13A15.34,15.34,0,0,0,216,201.16V87.84A15.34,15.34,0,0,0,200.66,72.5h0A15.35,15.35,0,0,0,187.58,79.81Z'/%3E%3C/g%3E%3C/svg%3E";
 
@@ -47,14 +45,23 @@ impl Contract {
             _ => Web4Response::Body {
                 content_type: "text/html; charset=UTF-8".to_owned(),
                 body: INDEX_HTML.to_owned(),
-            }
+            },
         }
-        
     }
 
-    /*#[private]
+    #[payable]
+    pub fn nft_mint(
+        &mut self,
+        token_id: TokenId,
+        token_owner_id: AccountId,
+        token_metadata: TokenMetadata,
+    ) -> Token {
+        assert_eq!(env::predecessor_account_id(), self.tokens.owner_id, "Unauthorized");
+        self.tokens.internal_mint(token_id, token_owner_id, Some(token_metadata))
+    }
+
     #[init(ignore_state)]
-    pub fn migrate() -> Self {
+    pub fn new() -> Self {
         Self {
             tokens: NonFungibleToken::new(
                 StorageKey::NonFungibleToken,
@@ -73,9 +80,16 @@ impl Contract {
                 reference_hash: None,
             })),
         }
-    } */
+    }
 }
 
 near_contract_standards::impl_non_fungible_token_core!(Contract, tokens);
 near_contract_standards::impl_non_fungible_token_approval!(Contract, tokens);
 near_contract_standards::impl_non_fungible_token_enumeration!(Contract, tokens);
+
+#[near_bindgen]
+impl NonFungibleTokenMetadataProvider for Contract {
+    fn nft_metadata(&self) -> NFTContractMetadata {
+        self.metadata.get().unwrap()
+    }
+}
