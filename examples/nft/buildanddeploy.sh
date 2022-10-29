24c1e804c3ac094c16d6c10913cd97bfc894cd84
Peter Salomonsen <petersalomonsen@runbox.com>
2022-10-29 19:59:54 +0000

    gitpod additions

--- examples/nft/buildanddeploy.sh
+++ examples/nft/buildanddeploy.sh
@@ -8,6 +8,7 @@ wasm-metadce -f meta-dce.json ../../target/wasm32-unknown-unknown/release/quickj
 wasm-opt -Oz out/nft.wasm -o out/nft.wasm
 if [ -z "$1" ]
 then
+    echo "Deploying to DEV account"
     near dev-deploy out/nft.wasm --initFunction=new --initArgs '{}'
 else
     # Deploy to account given in argument to this script
