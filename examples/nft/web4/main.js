0eb43500c4644d228dd254cbceac9a6e799df490
Peter Salomonsen <pjsalomonsen@gmail.com>
2022-11-19 17:34:49 +0100

    Show clickable list of songs changing player time

--- examples/nft/web4/main.js
+++ examples/nft/web4/main.js
@@ -23,9 +23,16 @@ if ('serviceWorker' in navigator) {
         }
 
         const musicwasms = [
-            { path: 'musicwasms/fall.wasm', durationSeconds: 60 },
-            { path: 'musicwasms/noiseandmadness.wasm', durationSeconds: 60 },
-            { path: 'musicwasms/goodtimes.wasm', durationSeconds: 60 }
+            { name: 'Groove is in the code', path: 'musicwasms/grooveisinthecode.wasm', durationSeconds: 154 },
+            { name: 'Fall', path: 'musicwasms/fall.wasm', durationSeconds: 174 },
+            { name: 'Noise and madness', path: 'musicwasms/noiseandmadness.wasm', durationSeconds: 163 },
+            { name: 'Web chip-music', path: 'musicwasms/webchipmusic.wasm', durationSeconds: 133 },
+            { name: 'Wasm song', path: 'musicwasms/wasmsong.wasm', durationSeconds: 232 },
+            { name: 'Good times', path: 'musicwasms/goodtimes.wasm', durationSeconds: 176 },
+            { name: 'WebAssembly summit 1', path: 'musicwasms/wasmsummit1.wasm', durationSeconds: 154 },
+            { name: 'First attempt', path: 'musicwasms/firstattempt.wasm', durationSeconds: 217 },
+            { name: 'Shuffle chill', path: 'musicwasms/shufflechill.wasm', durationSeconds: 131 },
+            { name: 'WebAssembly summit 2', path: 'musicwasms/wasmsummit2.wasm', durationSeconds: 191 }
         ];
 
         let loadSuccessful = true;
@@ -50,8 +57,18 @@ if ('serviceWorker' in navigator) {
 
         const messageArea = document.getElementById('message');
         if (loadSuccessful) {
-            document.getElementById('player').src = 'music.wav';
-            messageArea.innerHTML = location.search.match(/message=([^&]+)/)[1];
+            const playerElement = document.getElementById('player');
+            playerElement.src = 'music.wav';
+            let pos = 0;
+            messageArea.innerHTML = musicwasms.map((song) => {
+                const jsondateminutesstart = 'yyyy-MM-dd HH:'.length;
+                const timestring = new Date(pos * 1000).toJSON().substring(jsondateminutesstart, jsondateminutesstart + 5);
+
+                let ret = `<a onclick="document.getElementById('player').currentTime=${pos}">${timestring} ${song.name}</a><br />`
+                pos += song.durationSeconds;
+
+                return ret;
+            }).join('\n') + location.search.match(/message=([^&]+)/)[1];
         } else {
             messageArea.innerHTML = 'unable to load music. check that you have a valid link signed by the NFT owner';
         }
