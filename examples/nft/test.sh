e5bd4dd24ba4e1885dd450802d3a8c835def3da2
Peter Salomonsen <pjsalomonsen@gmail.com>
2022-12-03 07:06:55 +0100

    use cargo wasi test, notes on wasm test limitations

--- examples/nft/test.sh
+++ examples/nft/test.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-set -e
-rm -f ../../target/wasm32-wasi/debug/deps/*.wasm
-RUSTFLAGS='-C link-args=--initial-memory=67108864' cargo test --target=wasm32-wasi --no-run
-WASMTIME_BACKTRACE_DETAILS=1 $HOME/.wasmtime/bin/wasmtime ../../target/wasm32-wasi/debug/deps/quickjs_rust_near_nft-*.wasm -- --show-output
+RUSTFLAGS='-C link-args=--initial-memory=67108864' cargo wasi test -- --show-output --nocapture
