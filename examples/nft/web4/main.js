fda27de58efcc343d6e6f895512f759f27ed2bd9
Peter Salomonsen <pjsalomonsen@gmail.com>
2022-12-25 17:13:44 +0100

    show nft owner thumbnail from social.near

--- examples/nft/web4/main.js
+++ examples/nft/web4/main.js
@@ -1,3 +1,5 @@
+import { getNearSocialProfile } from './nearsocial.js';
+
 (async () => {
     if ('serviceWorker' in navigator) {
         const reg = navigator.serviceWorker.register('serviceworker.js', { scope: './' })
@@ -88,10 +90,20 @@
         const nftowners = await (await fetch(new URL('nftowners.json?t=' + new Date().getTime(), import.meta.url))).json();
         const nftownerstablebody = document.getElementById('nftownerstablebody');
 
-        nftownerstablebody.innerHTML = nftowners.sort((a,b)=> parseInt(a.token_id) - parseInt(b.token_id)).map(o => `<tr>
-            <td>${o.token_id}</td>
-            <td><a href="https://near.social/#/mob.near/widget/ProfilePage?accountId=${o.owner_id}" target="_blank">${o.owner_id}</a></td>
-        </tr>`).join('');
+        nftowners.sort((a,b)=> parseInt(a.token_id) - parseInt(b.token_id));
+        
+        nftownerstablebody.innerHTML = nftowners.map((o, ndx) => `<tr>
+                <td>${o.token_id}</td>
+                <td><a href="https://near.social/#/mob.near/widget/ProfilePage?accountId=${o.owner_id}" target="_blank">${o.owner_id}</a></td>
+                <td><img id="${o.owner_id}_profileimage_${ndx}"></td>
+            </tr>`).join('');
+        
+        nftowners.forEach(async (o, ndx) => {
+            const profile = await getNearSocialProfile(o.owner_id);
+            if (profile[o.owner_id] && profile[o.owner_id].profile.image.ipfs_cid) {
+                document.getElementById(o.owner_id+'_profileimage_'+ndx).src= `https://i.near.social/thumbnail/https://ipfs.near.social/ipfs/${profile[o.owner_id].profile.image.ipfs_cid}`;
+            }
+        });
     } else {
         document.documentElement.innerHTML = 'This app requires that your browser supports serviceworkers';
     }
