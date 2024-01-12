61787f0d1165838a6cd9a39d787d1e0be29b27ae
Peter Salomonsen <pjsalomonsen@gmail.com>
2024-01-12 19:53:36 +0000

    dev container and optimizations

--- buildanddeploy.sh
+++ buildanddeploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 # Build for WebAssembly target
-RUSTFLAGS='-C link-arg=-s' cargo build --target=wasm32-unknown-unknown --release
+cargo build --target=wasm32-unknown-unknown --release
 # Remove unneeded WebAssembly exports
 wasm-metadce -f meta-dce.json target/wasm32-unknown-unknown/release/quickjs_rust_near.wasm -o out/main.wasm
 # Optimize the Wasm binary
-wasm-opt -Oz --signext-lowering out/main.wasm -o out/main.wasm
+wasm-opt --converge -Oz --signext-lowering out/main.wasm -o out/main.wasm
 if [ -z "$1" ]
 then
     # Deploy to DEV account
