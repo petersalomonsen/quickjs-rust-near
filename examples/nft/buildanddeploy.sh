d4d4d23b75e53d99afa9958fc24114da37479728
Peter Salomonsen <pjsalomonsen@gmail.com>
2024-07-11 09:07:34 +0000

    mkdir out if not exists

--- examples/nft/buildanddeploy.sh
+++ examples/nft/buildanddeploy.sh
@@ -3,6 +3,7 @@ set -e
 # Build for WebAssembly target
 cargo build --target=wasm32-unknown-unknown --release
 # Remove unneeded WebAssembly exports
+mkdir -p out
 wasm-metadce -f meta-dce.json ../../target/wasm32-unknown-unknown/release/quickjs_rust_near_nft.wasm -o out/nft.wasm
 # Optimize the Wasm binary
 wasm-opt --converge -Oz --signext-lowering out/nft.wasm -o out/nft.wasm
