f5cc66d03f39216d5351089841259fd5052c90e2
Peter Salomonsen <pjsalomonsen@gmail.com>
2024-07-21 16:53:47 +0000

    fix test-web

--- web4/webassemblymusic/musictemplate.test.js
+++ web4/webassemblymusic/musictemplate.test.js
@@ -1,18 +1,20 @@
-import { wrapJSmusicInTemplate } from './musictemplate.js';
+import { wrapJSmusicInTemplate } from './musictemplate.js';
 import { readFile } from 'fs/promises';
+import { test } from 'node:test';
+import { expect } from 'chai';
 
 test('should create a javascript function that generates music', async () => {
     const musicsource = await readFile(new URL('sample_music2.js', import.meta.url));
-    const musicfunctionsource = wrapJSmusicInTemplate(musicsource);
+    const musicfunctionsource = wrapJSmusicInTemplate(musicsource);
 
     const musicfunction = new Function(musicfunctionsource);
     const generatedMusic = await new Promise(resolve => {
         globalThis.env = {
-            input: () => JSON.stringify({bpm: 90}),
+            input: () => JSON.stringify({ bpm: 90 }),
             value_return: (val) => resolve(val)
         }
         musicfunction();
     });
 
-    expect(JSON.parse(generatedMusic).length).toBe(115);
+    expect(JSON.parse(generatedMusic).length).to.equal(115);
 });
