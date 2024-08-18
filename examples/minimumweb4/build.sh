ed457942bf9fd896feabe98871ca935d9f4b3bc2
Peter Salomonsen <pjsalomonsen@gmail.com>
2024-08-18 16:19:46 +0200

    gh action for releasing example contracts (#17)
    Add a release with a zip file of example contract wasm builds.

--- examples/minimumweb4/build.sh
+++ examples/minimumweb4/build.sh
@@ -2,6 +2,7 @@
 set -e
 # Build for WebAssembly target
 RUSTFLAGS='-C link-arg=-s' cargo build --target=wasm32-unknown-unknown --release
+mkdir -p out
 # Remove unneeded WebAssembly exports
 wasm-metadce -f meta-dce.json ../../target/wasm32-unknown-unknown/release/quickjs_rust_near_minimum_web4.wasm -o out/minimum_web4.wasm
 # Optimize the Wasm binary
