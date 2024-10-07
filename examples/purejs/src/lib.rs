47a350e6c5ae0cc72f246666c64bc042b25d3625
Peter Salomonsen <pjsalomonsen@gmail.com>
2024-10-07 18:14:30 +0200

    no need to create runtime, as it already happens when loading bydecode

--- examples/purejs/src/lib.rs
+++ examples/purejs/src/lib.rs
@@ -1,7 +1,7 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::near_bindgen;
+use near_sdk::{env, near_bindgen};
 use quickjs_rust_near::jslib::{
-    js_call_function, load_js_bytecode, create_runtime
+    js_call_function, load_js_bytecode
 };
 
 #[near_bindgen]
@@ -11,8 +11,7 @@ pub struct Contract {}
 #[near_bindgen]
 impl Contract {
     pub fn some_js_function(&self) {
-        unsafe {
-            create_runtime();
+        unsafe {            
             let jsmod = load_js_bytecode(123456789 as *const u8, 987654321);            
             js_call_function(jsmod, 456123987 as i32);
         }
