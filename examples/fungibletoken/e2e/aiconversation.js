4a38d5ea9b3e79eccee06cc3eca4ce19346747ab
Peter Salomonsen <pjsalomonsen@gmail.com>
2024-12-14 17:09:28 +0100

    AI proxy spin app (#23)
    - copied code from https://github.com/petersalomonsen/near-openai/pull/11, which adds a [spin](https://github.com/fermyon/spin) app with a proxy to the OpenAI API, using the Fungible Token contract to cover usage cost
    - hash `conversation_id` when calling the smart contract from the proxy to ensure that the smart contract does not reveal the conversation id that the AI proxy accepts
    - added a test for the [aiconversation.js](https://github.com/petersalomonsen/quickjs-rust-near/blob/master/examples/fungibletoken/e2e/aiconversation.js) script that also shows the full process of starting a conversation and to get the refund when done

--- examples/fungibletoken/e2e/aiconversation.js
+++ examples/fungibletoken/e2e/aiconversation.js
@@ -1,8 +1,17 @@
 export function start_ai_conversation() {
     const amount = 128_000_000n;
-    let conversation_id = env.signer_account_id() + "_" + (new Date().getTime());
+    const { conversation_id } = JSON.parse(env.input());
+    if (!conversation_id) {
+        env.panic(`must provide conversation_id`);
+        return;
+    }
+    const existing_conversation = env.get_data(conversation_id);
+    if (existing_conversation) {
+        env.panic(`conversation already exist`);
+        return;
+    }
     env.set_data(conversation_id, JSON.stringify({ receiver_id: env.signer_account_id(), amount: amount.toString() }));
-    env.ft_transfer_internal(env.signer_account_id(), 'aitoken.testnet', amount.toString());
+    env.ft_transfer_internal(env.signer_account_id(), env.current_account_id(), amount.toString());
     env.value_return(conversation_id);
 }
 
@@ -13,16 +22,21 @@ export function view_ai_conversation() {
 
 export function refund_unspent() {
     const { refund_message, signature } = JSON.parse(env.input());
-    const public_key = new Uint8Array([74, 228, 41, 15, 116, 28, 61, 128, 166, 59, 142, 157, 249, 47, 117, 247, 100, 245, 49, 238, 11, 198, 191, 189, 249, 115, 108, 241, 114, 247, 26, 166]);
+    const public_key = new Uint8Array(REPLACE_REFUND_SIGNATURE_PUBLIC_KEY);
 
     const signature_is_valid = env.ed25519_verify(new Uint8Array(signature), new Uint8Array(env.sha256_utf8(refund_message)), public_key);
+
     if (signature_is_valid) {
         const { receiver_id, refund_amount, conversation_id } = JSON.parse(refund_message);
+
         const conversation_data = JSON.parse(env.get_data(conversation_id));
+
         if (BigInt(conversation_data.amount) > BigInt(refund_amount)) {
             env.clear_data(conversation_id);
-            env.ft_transfer_internal('aitoken.testnet', receiver_id, refund_amount);
+            env.ft_transfer_internal(env.current_account_id(), receiver_id, refund_amount);
             print(`refunded ${refund_amount} to ${receiver_id}`);
         }
+    } else {
+        env.panic("Invalid signature");
     }
 }
---
+++ examples/fungibletoken/e2e/aiconversation.test.js
@@ -0,0 +1,114 @@
+import { connect, keyStores } from 'near-api-js';
+import { Worker } from 'near-workspaces';
+import { before, after, test, describe, afterEach } from 'node:test';
+import { expect } from 'chai';
+import { createHash } from 'crypto';
+import { readFile } from 'fs/promises';
+
+const connectionConfig = {
+    networkId: "sandbox",
+    keyStore: new keyStores.InMemoryKeyStore(),
+    nodeUrl: "https://rpc.testnet.near.org"
+};
+
+describe('Fungible token contract', { only: true }, () => {
+    /**
+     * @type {Worker}
+     */
+    let worker;
+
+    /**
+     * @type {Account}
+     */
+    let root;
+
+    /**
+     * @type {import('near-workspaces').NearAccount}
+     */
+    let contract;
+    /**
+     * @type {import('near-workspaces').NearAccount}
+     */
+    let alice;
+
+    /**
+     * @type {import('near-workspaces').KeyPair}
+     */
+    let contractAccountKeyPair;
+
+    before(async () => {
+
+        worker = await Worker.init();
+
+        connectionConfig.nodeUrl = worker.provider.connection.url;
+        root = worker.rootAccount;
+
+        alice = await worker.rootAccount.createAccount('alice.test.near');
+
+        contract = await root.devDeploy('out/fungible_token.wasm');
+        await contract.call(contract.accountId, 'new_default_meta', { owner_id: contract.accountId, total_supply: 1_000_000_000_000n.toString() });
+        contractAccountKeyPair = await contract.getKey();
+        connectionConfig.keyStore.setKey("sandbox", contract.accountId, contractAccountKeyPair);
+        await alice.call(contract.accountId, 'storage_deposit', {
+            account_id: 'alice.test.near',
+            registration_only: true,
+        }, {
+            attachedDeposit: 1_0000_0000000000_0000000000n.toString()
+        });
+    });
+    after(async () => {
+        await worker.tearDown();
+    });
+
+    test('should run custom javascript transfer functions in contract with function access keys, and without attaching deposits', { only: true }, async () => {
+        const nearConnection = await connect(connectionConfig);
+        const accountId = contract.accountId;
+
+        const account = await nearConnection.account(accountId);
+        const javascript = (await readFile(new URL('aiconversation.js', import.meta.url))).toString()
+            .replace("REPLACE_REFUND_SIGNATURE_PUBLIC_KEY", JSON.stringify(Array.from((await contract.getKey()).getPublicKey().data)));
+
+        const real_conversation_id = `alice.test.near_${new Date().getTime()}`;;
+        const conversation_id = createHash('sha256').update(Buffer.from(real_conversation_id, 'utf8')).digest('hex');
+
+        await account.functionCall({
+            contractId: accountId,
+            methodName: 'post_javascript',
+            gas: '300000000000000',
+            args: {
+                javascript
+            }
+        });
+
+        await contract.call(accountId, 'ft_transfer', {
+            receiver_id: 'alice.test.near',
+            amount: 128_000_000n.toString(),
+        }, {
+            attachedDeposit: 1n.toString()
+        });
+
+        await alice.call(accountId, 'call_js_func', {
+            function_name: "start_ai_conversation",
+            conversation_id
+        });
+
+        expect(await contract.view('ft_balance_of', { account_id: 'alice.test.near' })).to.equal(0n.toString());
+        const conversation_data = await contract.view('view_js_func', { function_name: "view_ai_conversation", conversation_id });
+
+        expect(conversation_data.receiver_id).to.equal('alice.test.near');
+        expect(conversation_data.amount).to.equal(128_000_000n.toString());
+
+        const refund_message = JSON.stringify({ receiver_id: 'alice.test.near', refund_amount: 1000n.toString(), conversation_id });
+        const refund_message_hashed = createHash('sha256').update(Buffer.from(refund_message, 'utf8')).digest();
+        const signature = (await contract.getKey()).sign(Uint8Array.from(refund_message_hashed));
+
+        await alice.call(accountId, 'call_js_func',
+            {
+                function_name: "refund_unspent",
+                signature: Array.from(signature.signature),
+                refund_message
+            });
+
+        expect(await contract.view('ft_balance_of', { account_id: 'alice.test.near' })).to.equal(1_000n.toString());
+    });
+});
