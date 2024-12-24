521676f61f852c2e0553a47b6a828d16e0f11212
Peter Salomonsen <pjsalomonsen@gmail.com>
2024-12-24 15:38:06 +0100

    start test conversation from the browser

--- examples/aiproxy/playwright-tests/aiproxy.spec.js
+++ examples/aiproxy/playwright-tests/aiproxy.spec.js
@@ -2,13 +2,22 @@
 import { test, expect } from '@playwright/test';
 
 test('ask question', async ({ page }) => {
-  await page.goto('/');
+  const {functionAccessKeyPair, publicKey, accountId, contractId} = await fetch('http://localhost:14501').then(r => r.json());
 
-  const {real_conversation_id} = await fetch('http://localhost:14501').then(r => r.json());
-  await page.getByPlaceholder('conversation id').fill(real_conversation_id);
+  await page.goto('/');
+  await page.evaluate(({accountId, publicKey, functionAccessKeyPair, contractId}) => {
+    localStorage.setItem("aiproxy_wallet_auth_key", JSON.stringify({accountId, allKeys: [publicKey]}));
+    localStorage.setItem(`near-api-js:keystore:${accountId}:sandbox`, functionAccessKeyPair);
+    localStorage.setItem(`contractId`, contractId);
+  }, {accountId, publicKey, functionAccessKeyPair, contractId});
+
+  await page.reload();
+
+  await page.getByRole('button', { name: 'Start conversation' }).click();
 
   const questionArea = await page.getByPlaceholder('Type your question here...');
+  await expect(questionArea).toBeEnabled();
   questionArea.fill("Hello!");
-  await page.getByRole('button', { name: 'Ask OpenAI' }).click();
+  await page.getByRole('button', { name: 'Ask AI' }).click();
   await expect(await page.getByText("I am just a mockserver")).toBeVisible();
 });
