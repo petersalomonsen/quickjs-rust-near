2474854e98919490cbadcb190d2e21df4bd2906a
Peter Salomonsen <pjsalomonsen@gmail.com>
2025-02-16 16:01:13 +0100

    run javascript inside wasm, inside web4 contract

--- examples/aiproxy/web/openai/tools.js
+++ examples/aiproxy/web/openai/tools.js
@@ -1,26 +1,46 @@
+import { getContractInstanceExports } from "../contract-runner/contract-runner.js";
+
 export const toolImplementations = {
-    "run_javascript": async function(source) {    
-        return JSON.stringify(await eval(source));
-    }
+  "run_javascript": async function ({script}) {
+    const wasmbinary = new Uint8Array(await fetch('https://ipfs.web4.near.page/ipfs/bafkreigjjyocek3mdqk6rilzaxleg2swuka2nhzfx2gq4u7yicgdmvlh2a?filename=minimum_web4.wasm').then(r => r.arrayBuffer()));
+
+    const { exports, nearenv } = await getContractInstanceExports(wasmbinary);
+    nearenv.set_args({
+      javascript: `export async function custom() {
+          env.value_return(
+            JSON.stringify(await (async () => {
+                ${script}
+              })()
+            )
+          );
+        }
+        `
+    });
+    exports.post_javascript();
+    nearenv.set_args({ "function_name": "custom" });
+    exports.call_js_func();
+    return nearenv.latest_return_value;
+  }
 }
 
 export const tools = [
   {
-    type: "function",
-    function: {
-      name: "run_javascript",
-      description: "Run javascript snippet in a secure sandbox where there is no access to NPM libraries, NodeJS APIs or web APIs.",
-      parameters: {
-        type: "object",
-        properties: {
+    "type": "function",
+    "function": {
+      "name": "run_javascript",
+      "description": "Run javascript snippet in a secure sandbox where there is no access to NPM libraries, NodeJS APIs or web APIs. For the result to be passed correctly the snippet needs to end with a `return` statement with the result value.",
+      "parameters": {
+        "type": "object",
+        "properties": {
           "script": {
             "type": "string",
-            "description": "Javascript code"
-        }},
-        additionalProperties: false,
-        required: ["script"],
+            "description": "Javascript snippet"
+          }
+        },
+        "additionalProperties": false,
+        "required": ["script"],
       },
-      strict: true,
-    },
+      "strict": true
+    }
   }
 ];
