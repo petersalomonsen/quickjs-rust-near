670f65f7e3346ba2fa0b18de1ffecb3019fabbd3
Peter Salomonsen <pjsalomonsen@gmail.com>
2025-02-16 18:47:23 +0100

    run tests headless

--- examples/aiproxy/playwright.config.js
+++ examples/aiproxy/playwright.config.js
@@ -34,7 +34,7 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: 'http://127.0.0.1:8080',
     video: 'off',
-    headless: false,
+    headless: true,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
